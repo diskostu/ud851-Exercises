3057222a26a9e2d9e72a5a108378827dc1186c37
Denny Kluge <denny.kluge@gmail.com>
2020-09-23 10:07:57 +0200

    T06.01 done

---
+++ Lesson06-Visualizer-Preferences/T06.01-Exercise-SetupTheActivity/app/src/main/java/android/example/com/visualizerpreferences/SettingsActivity.java
@@ -0,0 +1,43 @@
+package android.example.com.visualizerpreferences;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    private final static String TAG = SettingsActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        final ActionBar actionBar = this.getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+
+    /**
+     * I *guess* this works like this: when the SettingsActivity is not started from the parent, but from, say, a
+     * notification, then clicking the back arrow in the action bar would close the app, as it is a simple "back
+     * button" behaviour.
+     * With this additional code here, we say, hey, if you press the back arrow in the action bar, always go back to
+     * the parent activity. We defined parent-child-activity-behaviour in the manifest.
+     */
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        final int itemId = item.getItemId();
+        if (itemId == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
