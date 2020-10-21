c8b5e3931499081db1987cc2167579c019ed2c26
Denny Kluge <denny.kluge@gmail.com>
2020-10-21 09:02:55 +0200

    T07.01 done

--- app/src/test/java/com/example/android/waitlist/ContractClassUnitTest.java
+++ app/src/test/java/com/example/android/waitlist/ContractClassUnitTest.java
@@ -8,8 +8,11 @@ import org.junit.Test;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -41,12 +44,18 @@ public class ContractClassUnitTest {
         assertEquals("Cannot find inner class to complete unit test", 1, innerClasses.length);
         Class entryClass = innerClasses[0];
         Field[] allFields = entryClass.getDeclaredFields();
-        assertEquals("There should be exactly 4 String members in the inner class", 4, allFields.length);
-        for (Field field : allFields) {
-            assertTrue("All members in the contract class should be Strings", field.getType()==String.class);
+
+        final List<Field> allFields2 = new ArrayList<>();
+        for (final Field field : allFields) {
+            if (!field.getType().equals(WaitlistContract.class)) {
+                allFields2.add(field);
+            }
+        }
+        assertEquals("There should be exactly 4 String members in the inner class", 4, allFields2.size());
+        for (Field field : allFields2) {
+            assertSame("All members in the contract class should be Strings", field.getType(), String.class);
             assertTrue("All members in the contract class should be final", Modifier.isFinal(field.getModifiers()));
             assertTrue("All members in the contract class should be static", Modifier.isStatic(field.getModifiers()));
         }
     }
-
 }
